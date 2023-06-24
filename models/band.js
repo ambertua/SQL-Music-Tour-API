12a398fdd37c1fecf770476a717a65096159de08
ambertua <amber.tuamoheloa@yahoo.com>
2023-06-24 12:41:48 -0700

    added association queries

--- models/band.js
+++ models/band.js
@@ -9,8 +9,15 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({ MeetGreet, }) {
-      // define association here
+    static associate({ MeetGreet, SetTime }) {
+      Band.hasMany(MeetGreet, {
+        foreignKey: 'band_id',
+        as: "meet_greets"
+      })
+      Band.hasMany(SetTime, {
+        foreignKey: 'band_id',
+        as: 'set_times'
+      })
     }
   }
   Band.init({
@@ -38,9 +45,8 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Band',
-    tableName: 'bands',
+    tableName: 'band',
     timestamps: false
   })
-;
   return Band;
 };
